516aea5b21f71aaa25ca5d0880fc081c86b79027
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2023-11-22 20:47:59 +0500

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -21,13 +21,13 @@ async def cmd_warmane(msg: types.Message):
         async with session.get('https://www.warmane.com/') as response:
             text = await response.text(encoding="utf-8")
             soup = BeautifulSoup(text, "html.parser")
-            l = soup.find_all("span")[0].get_text()
-            i = soup.find_all("span")[1].get_text()
-            b = soup.find_all("span")[2].get_text()
-            f = soup.find_all("span")[3].get_text()
-            t = soup.find_all("span")[4].get_text()
-            await msg.reply(f"♻ Warmane online:\n• Lordaeron {l}\n• Icecrown {i}\n• Blackrock {b}\n• Frostwolf {f}\nJami online: {t}")
-
+            o = soup.find_all("span")[0].get_text()
+            l = soup.find_all("span")[1].get_text()
+            i = soup.find_all("span")[2].get_text()
+            b = soup.find_all("span")[3].get_text()
+            f = soup.find_all("span")[4].get_text()
+            all = soup.find_all("span")[5].get_text()
+            await msg.reply(f"♻ Warmane online:\n• Onyxia {o}\n• Lordaeron {l}\n• Icecrown {i}\n• Blackrock {b}\n• Frostwolf {f}\nJami online: {all}")
 
 @router.message(Command("circle"))
 async def cmd_circle(msg: types.Message):
