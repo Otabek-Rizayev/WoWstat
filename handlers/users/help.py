ac708c2c20cd9b9a3be85e736a72c2dd0be8fc08
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2023-11-25 15:14:59 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -7,9 +7,10 @@ router = Router()
 @router.message(Command('help'))
 async def bot_help(message: types.Message):
     text = ("Buyruqlar: ",
-            "/online - Battle-arena.uz serveri barcha realmidagi onlaynni ko'rsatadi."
+            "/online - Battle-arena.uz serveri barcha realmidagi onlaynni ko'rsatadi.",
             "/warmane - Warmane.com serveri barcha realmidagi onlaynni ko'rsatadi.",
             "/circle - WoWCircle.com serveri barcha realmidagi onlaynni ko'rsatadi.",
-            "/sirus - Sirus.one serveri barcha realmidagi onlaynni ko'rsatadi."
-            "/item - WoW 3.3.5 versiyasidagi xohlagan buyumlarni qidirib topish.")
+            "/sirus - Sirus.one serveri barcha realmidagi onlaynni ko'rsatadi.",
+            "/item - WoW 3.3.5 versiyasidagi xohlagan buyumlarni qidirib topish.",
+           )
     await message.answer(text="\n".join(text))
