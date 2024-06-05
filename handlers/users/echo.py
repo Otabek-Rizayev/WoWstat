d304df8b4d495ae8975c5c913c2d5e14cbb5a8f5
Otabek Rizayev <46789195+Otabek-Rizayev@users.noreply.github.com>
2024-06-05 16:19:30 +0500

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,10 +2,34 @@ from aiogram import Router, types
 from aiogram import types
 import aiohttp
 import asyncio
-from scrape import off_func
+#from scrape import off_func
 from bs4 import BeautifulSoup
 from aiogram.filters import Command
 router = Router()
+import cloudscraper
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text(encoding="utf-8")
+
+async def off_func(url):
+    scraper = cloudscraper.create_scraper()
+    headers = scraper.headers
+    cookies = scraper.cookies
+
+    async with aiohttp.ClientSession(headers=headers, cookies=cookies) as session:
+        content = await fetch(session, "https://www.wowprogress.com/realms/rank/eu")
+        soup = BeautifulSoup(content, "html.parser")
+        tr = soup.find_all("tr")
+        td = soup.find_all("td")
+        table = soup.find_all("table", attrs={"class":"realm_list"})
+        data = []
+        for tb in table:
+            trs = tb.find_all("tr")
+            for i, tds in enumerate(trs):
+                meta = tds.find_all("td")
+                if i > 0 and i< 8:
+                        await return f"ℹ {meta[1].text}| ♻️ {meta[5].text} | ⚔ {meta[2].text} | 🌐 {meta[3].text}"
 
 @router.message(Command("online"))
 async def cmd_wowgame(msg: types.Message):
